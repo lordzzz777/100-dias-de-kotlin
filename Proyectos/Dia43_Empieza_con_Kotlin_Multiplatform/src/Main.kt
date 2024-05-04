a585029bba57265c95c6c7d77fc74b677153c6ae
lordzzz777 <lordzzz8@gmail.com>
2024-05-04 09:26:59 +0200

    Día 43, Empieza con Kotlin Multiplatform

---
+++ Proyectos/Dia43_Empieza_con_Kotlin_Multiplatform/src/Main.kt
@@ -0,0 +1,52 @@
+// ------------ Empieza con Kotlin Multiplatform ------------
+
+// El soporte para la programación multiplataforma es uno de los beneficios clave de Kotlin.
+// Reduce el tiempo dedicado a escribir y mantener el mismo código para diferentes plataformas,
+// al tiempo que conserva la flexibilidad y los beneficios de la programación nativa.
+
+//Obtenga más información sobre los beneficios multiplataforma de Kotlin.
+
+// ------------ Empieza desde cero ------------
+
+// Empieza con Kotlin Multiplatform. Crea tu primera aplicación multiplataforma que funcione
+// en Android e iOS con la ayuda del complemento móvil multiplataforma de Kotlin para Android
+// Studio. Aprende a crear, ejecutar y añadir dependencias a las aplicaciones móviles
+// multiplataforma.
+
+// Comparte interfaces de usuario entre iOS y Android. Crea una aplicación multiplataforma
+// Kotlin que utilice el marco de interfaz de usuario multiplataforma Compose para compartir
+// la lógica empresarial y la interfaz de usuario entre las plataformas iOS, Android y de
+// escritorio.
+
+// ------------ Sumérgete en Kotlin Multiplatform ------------
+
+// Una vez que haya adquirido algo de experiencia con Kotlin Multiplatform y quiera saber
+// cómo resolver tareas particulares de desarrollo multiplataforma:
+
+// ----------- Comparte código en las plataformas de tu proyecto multiplataforma de Kotlin.------------
+
+// Conéctese a API específicas de la plataforma al desarrollar aplicaciones y bibliotecas
+// multiplataforma.
+
+// Configure objetivos manualmente para su proyecto multiplataforma Kotlin.
+
+// Añade dependencias en el estándar, la prueba u otra biblioteca de kotlinx.
+
+// Configure compilaciones para fines de producción y prueba en su proyecto.
+
+// Publica una biblioteca multiplataforma en el repositorio Maven.
+
+// Crea binarios nativos como ejecutables o bibliotecas compartidas,
+// como marcos universales o XCFrameworks.
+
+// ------------ Obtener ayuda -------------
+
+// Kotlin Slack: Consigue una invitación y únete al canal #multiplataforma
+
+// StackOverflow: Suscríbete a la etiqueta "kotlin-multiplataforma"
+
+// Rastreador de problemas de Kotlin: Informar de un nuevo problema
+
+fun main() {
+
+}
